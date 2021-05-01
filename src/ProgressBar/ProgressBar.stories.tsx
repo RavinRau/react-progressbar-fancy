23ecb57d013f5658090cb12f3eb2918554b57d80
RavinRau <ravinrau@accendo.com.my>
2021-05-02 01:47:27 +0800

    did some correction in storybook that was causing error.

--- src/ProgressBar/ProgressBar.stories.tsx
+++ src/ProgressBar/ProgressBar.stories.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import {ProgressBar} from "./ProgressBar";
+import * as React from "react";
+import ProgressBar from "./ProgressBar";
 
 export default {
     title: "ProgressBar"
